7b1684eecfaa931975fdb8db074137fbc44e7069
Peregud Oxana <ox.peregud@gmail.com>
2022-03-29 22:56:26 +0300

    add course goals app

---
+++ course-goals-app/src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './CourseGoalList.module.css';
+import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
+
+const CourseGoalList = props => {
+
+    return (
+        <ul className={styles.goalList}>
+            {props.items.map(goal => (
+                <CourseGoalItem
+                    key={goal.id}
+                    id={goal.id}
+                    onDelete={props.onDeleteItem}>
+                    {goal.text}
+                </CourseGoalItem>
+            ))}
+        </ul>
+    );
+};
+
+export default CourseGoalList;

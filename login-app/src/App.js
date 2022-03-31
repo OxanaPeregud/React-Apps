f35f1e5106cfc2b9c69fe2e6e8fad10ab719a5ac
Peregud Oxana <ox.peregud@gmail.com>
2022-03-31 11:08:52 +0300

    feat: added custom context provider component

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from "./store/auth-context";
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
         }
     }, []);
 
-    const loginHandler = (email, password) => {
+    const loginHandler = () => {
         localStorage.setItem('isLoggedIn', '1');
         setIsLoggedIn(true);
     };
@@ -25,15 +26,19 @@ function App() {
     };
 
     return (
-        <>
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: isLoggedIn,
+                onLogout: logoutHandler
+            }}
+        >
             <MainHeader
-                isAuthenticated={isLoggedIn}
                 onLogout={logoutHandler}/>
             <main>
                 {!isLoggedIn && <Login onLogin={loginHandler}/>}
                 {isLoggedIn && <Home onLogout={logoutHandler}/>}
             </main>
-        </>
+        </AuthContext.Provider>
     );
 }
 
